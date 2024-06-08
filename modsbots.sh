4d3ba2a016e0adbeca40488ad95bf04d41876e23
PlayBillbes <131470638+PlayBillbes@users.noreply.github.com>
2024-06-08 14:02:40 +0630

    Update modsbots.sh

--- modsbots.sh
+++ modsbots.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-relay login -k c2b7743e-5963-4173-b8ba-1331c43d0542 -s kd0qC7L97JR9
+
+relay login -k 7af1e38b-1a65-42ed-a5bf-b982c14764ea -s oClcgeTXIDZo
 echo "ttyd serving at port 80 with username:pass as kali:kali"
 ./vless.sh bash
 nohup relay connect --region eu --name webig & ls
