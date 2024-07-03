866ebd8ed3b44db667d04a15d857e6ed54e99e1d
modszf <botsmods75@gmail.com>
2024-07-04 00:13:30 +0630

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,4 +17,4 @@ chmod +x web server
 sleep 5
 nohup ./web run &>/dev/null &
 sleep 5
-nohup ./server tunnel --edge-ip-version auto --no-autoupdate --protocol http2 run --token eyJhIjoiZmM5YWQ3MmI4ZTYyZGZkMzMxZTk1MjY3MjA1YjhmZGUiLCJ0IjoiZmViMGQ3ZGYtZDU0Yi00OTRlLTlkM2YtMTM3Mzc3OTUzY2Q3IiwicyI6Ik1EZGpOVGt3TlRZdFlqVXpZUzAwTXpSa0xUazRNVE10TkdNelltWXpaVFJqWlRWaiJ9 >/dev/null 2>&1 &
+nohup ./server tunnel --edge-ip-version auto --no-autoupdate --protocol http2 run --token eyJhIjoiZmM5YWQ3MmI4ZTYyZGZkMzMxZTk1MjY3MjA1YjhmZGUiLCJ0IjoiMTM4NDgwNjItZWQxOC00MDJhLWJlMGQtMDlhNGEyOGU4NzY5IiwicyI6IlpUZzNZVEExTlRFdFlqZ3pOQzAwWlRVMkxUZzVOR1F0WW1SaU1UWTBOelptTXpFMSJ9 >/dev/null 2>&1 &
